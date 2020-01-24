d7610307a5661d63c4a7ad42491967cdff571a79
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-01-24 09:32:44 +0200

    fix

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,8 @@
 const checkForSpam = function(message) {
-  return message.toLowerCase().includes('spam') ||
+  return (
+    message.toLowerCase().includes('spam') ||
     message.toLowerCase().includes('sale')
-    ? true
-    : false;
+  );
 };
 console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
