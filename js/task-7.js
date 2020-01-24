fd7e9744d6e26a33457073a121f449ca129d64dd
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-01-24 09:37:01 +0200

    fix

--- js/task-7.js
+++ js/task-7.js
@@ -1,6 +1,6 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const isLoginValid = function(login) {
-  return login.length >= 4 && login.length <= 16 ? true : false;
+  return login.length >= 4 && login.length <= 16;
 };
 const isLoginUnique = function(allLogins, login) {
   return !allLogins.includes(login);
